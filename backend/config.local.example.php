6bf1294bf37f75aae757de0c87173ed21b2fd667
wfuzatto <shockwave_acb@hotmail.com>
2026-09-21 09:52:12 -0300

    Add backend/config.local.example.php

---
+++ backend/config.local.example.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'database' => 'iplate',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8mb4',
+    ],
+    'hotelaria_db' => [
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'database' => 'hotelaria',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8mb4',
+    ],
+];
